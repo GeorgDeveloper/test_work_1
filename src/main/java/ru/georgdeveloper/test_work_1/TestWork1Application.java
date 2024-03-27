296d4ff8304861e655d742ed05f69621e61ee5f3
Georgvit <ifreyd@mail.ru>
2024-03-27 18:48:45 +0300

    Initial commit

---
+++ src/main/java/ru/georgdeveloper/test_work_1/TestWork1Application.java
@@ -0,0 +1,13 @@
+package ru.georgdeveloper.test_work_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWork1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWork1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/georgdeveloper/test_work_1/TestWork1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.georgdeveloper.test_work_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWork1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
