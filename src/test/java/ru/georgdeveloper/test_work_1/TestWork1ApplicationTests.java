399639c2652d736361b32f2d35898838b7c483b8
Georgvit <ifreyd@mail.ru>
2024-03-27 18:50:02 +0300

    create readme

--- src/test/java/ru/georgdeveloper/test_work_1/TestWork1ApplicationTests.java
+++ src/test/java/ru/georgdeveloper/test_work_1/TestWork1ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestWork1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
